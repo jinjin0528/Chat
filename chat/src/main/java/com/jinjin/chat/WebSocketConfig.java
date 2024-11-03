0c40fe9879678d7dde28e9ab835a677c57fe3a01
이효진 <jin12lee@naver.com>
2024-11-03 22:04:19 +0900

    feat: 채팅 config 설정

---
+++ chat/src/main/java/com/jinjin/chat/WebSocketConfig.java
@@ -0,0 +1,11 @@
+package com.jinjin.chat;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+}
