7081d9ca2bcfd776b715e4c4a5f413c88894590f
jinjin0528 <jin12lee@naver.com>
2024-11-04 21:30:12 +0900

    feat: 채팅 입력 완료. 양방향 유저 설정 해야 함

---
+++ chat/src/main/java/com/jinjin/chat/ChatController.java
@@ -0,0 +1,15 @@
+package com.jinjin.chat;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/send")
+    @SendTo("/topic/messages")
+    public ChatMessage sendMessage(ChatMessage message){
+        return message;
+    }
+}
---
+++ chat/src/main/java/com/jinjin/chat/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.jinjin.chat;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ChatMessage {
+    private String content;
+    private String sender;
+
+
+}
