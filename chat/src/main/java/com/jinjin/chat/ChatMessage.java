a7cd6719269499dd1354d60646fe104248762fb5
jinjin0528 <jin12lee@naver.com>
2024-11-05 17:41:24 +0900

    feat: mongoDB 설정

--- chat/src/main/java/com/jinjin/chat/ChatMessage.java
+++ chat/src/main/java/com/jinjin/chat/ChatMessage.java
@@ -1,15 +1,22 @@
 package com.jinjin.chat;
 
+import jakarta.persistence.Id;
 import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-@NoArgsConstructor
-@AllArgsConstructor
+import java.time.LocalDateTime;
+
+@Document(collection = "chat")
 @Getter
 @Setter
 @ToString
 public class ChatMessage {
+    @Id
+    private String chatId;
     private String content;
     private String sender;
+    private LocalDateTime sentAt;
+    private String chatRoomId;
 
 
 }
---
+++ chat/src/main/java/com/jinjin/chat/ChatRepository.java
@@ -0,0 +1,9 @@
+package com.jinjin.chat;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ChatRepository extends MongoRepository<ChatMessage, String> {
+    List<ChatMessage> findByChatRoomId(String chatRoomId);
+}
